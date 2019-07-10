819c0fca39970c3edba05b4446e66ab2f7b85252
xxihyun0830 <47096555+xxihyun0830@users.noreply.github.com>
2019-07-10 19:19:45 +0900

    Update 별11.java

--- A_0704/src/backjoon/별11.java
+++ A_0704/src/backjoon/별11.java
@@ -1,5 +1,36 @@
+    
 package backjoon;
+import java.util.*;
+public class 별11 {
 
-public class ��11 {
+	/////////나는 이해를 못 하겠다..ㅎ 프린트라도 해서 봐야 겠다...
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		String[] result = new String[n];
+		result[0] = "  *  ";
+		result[1] = " * * ";
+		result[2] = "*****";
 
+
+		String space = "   ";
+		if (n!=3) {
+			for (int i=3; i<n; i*=2) {
+				for (int j=i; j<i*2; j++) {
+					result[j] = result[j-i] + " " + result[j-i];
+				}
+				for (int k=0; k<i; k++) {
+					result[k] = space + result[k] + space;
+				}
+				space += space;
+			}
+
+		} 
+		for (int i=0; i<n; i++) {
+			System.out.println(result[i]);
+		}	
+	}
 }
+
