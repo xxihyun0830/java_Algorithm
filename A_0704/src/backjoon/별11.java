00fe7c5aca803a1a120d056c25c898c87e529599
LeeJiHyun <xxihyun0830@gmail.com>
2019-07-10 19:17:16 +0900

    별찍기 11

--- A_0704/src/backjoon/별11.java
+++ A_0704/src/backjoon/별11.java
@@ -1,5 +1,35 @@
 package backjoon;
-
+import java.util.*;
 public class 별11 {
 
+	/////////나는 이해를 못 하겠다..ㅎ 프린트라도 해서 봐야 겠다...
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		String[] result = new String[n];
+		result[0] = "  *  ";
+		result[1] = " * * ";
+		result[2] = "*****";
+
+
+		String space = "   ";
+		if (n!=3) {
+			for (int i=3; i<n; i*=2) {
+				for (int j=i; j<i*2; j++) {
+					result[j] = result[j-i] + " " + result[j-i];
+				}
+				for (int k=0; k<i; k++) {
+					result[k] = space + result[k] + space;
+				}
+				space += space;
+			}
+
+		} 
+		for (int i=0; i<n; i++) {
+			System.out.println(result[i]);
+		}	
+	}
 }
+
