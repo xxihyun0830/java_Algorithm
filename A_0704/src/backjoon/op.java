771f44577feaed6fb1717dd8c339717a99ead124
LeeJiHyun <xxihyun0830@gmail.com>
2019-07-10 19:28:10 +0900

    goog for

---
+++ A_0704/src/backjoon/op.java
@@ -0,0 +1,12 @@
+package backjoon;
+import java.util.*;
+public class op {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		sc.close();
+		System.out.println("hi"+ str);
+	}
+}
+
