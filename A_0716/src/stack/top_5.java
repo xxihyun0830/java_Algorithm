11ab8a3229e53e6bd19a3e8422b409dd0def6bc7
LeeJiHyun <xxihyun0830@gmail.com>
2019-08-21 17:24:16 +0900

    top 백준 2493...

--- A_0716/src/stack/top_5.java
+++ A_0716/src/stack/top_5.java
@@ -1,26 +1,58 @@
 package stack;
 
 import java.util.*;
+
+class top{
+	int index;
+	int value;
+	
+	public top(int index, int value) {
+		this.index = index;
+		this.value = value;
+	}
+}
+
 public class top_5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Stack<top> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
 		
 		int n = sc.nextInt();
-		Stack<Integer> stack = new Stack<>();
-		ArrayList<Integer> list = new ArrayList<>();
+		int index = 1;
 		
 		for(int i=0;i<n;i++) {
 			int m = sc.nextInt();
-			list.add(m); //indexOf를 사용하기 위해서 필요함 
-			System.out.println("_______________________________________");
-			System.out.println("curr-m :" + m);
 			
-					
 			if(stack.isEmpty()) {
-				stack.push(m);
-				System.out.println("-----0" + " first ");//
-				
+				stack.push(new top(index, m));
+				sb.append(0);
+				System.out.println("-->"+"0");
+				System.out.println("("+stack.peek().index + ", "+ stack.peek().value +")");
+				index ++;
+			}else { 
+				top t = stack.pop();
+				if(t.value > m) {
+					stack.push(t);
+					sb.append(t.index);
+					System.out.println("-->"+t.index);
+					System.out.println("("+stack.peek().index + ", "+ stack.peek().value +")");
+				}else if(t.value < m) {
+					//새로운 max의 등장
+					stack.push(new top(index, m));
+					sb.append(0);
+					System.out.println("-->"+"0");
+					System.out.println("("+stack.peek().index + ", "+ stack.peek().value +")");
+				}
+				index ++;
 			}
 		}
+		System.out.println("------------------------");
+		
+		for(int i = 0; i <n;i++) {
+			System.out.print(sb.charAt(i)+" ");
+		}
+		
+		sc.close();
 	}
 }
---
+++ A_0716/src/stack/top_6.java
@@ -0,0 +1,82 @@
+package stack;
+import java.util.*;
+
+/*class top {
+	int index;
+	int value;
+	
+	public top(int index, int value) {
+		this.index = index;
+		this.value = value;
+	}
+}*/
+
+public class top_6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList <Integer> list = new ArrayList<>();
+		Stack<top> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i<n;i++) {
+			int m = sc.nextInt();
+			list.add(m);
+		}
+		
+		for(int i = 0; i<n;i++) {
+			if(i == 0) {
+				stack.push(new top(i+1, list.get(i)));
+				System.out.println("curr-value :" + list.get(i)+" ->"+"0");
+				sb.append("0");
+			}else {
+				if(list.get(i-1) < list.get(i)) {
+					top t = stack.pop();
+					if(t.value > list.get(i)) {
+						stack.push(t);
+						System.out.println("curr-value :" + list.get(i)+"->"+t.index);
+					    sb.append(t.index);
+					    
+					}else if(t.value < list.get(i)) {//새로운 max의 등장
+						stack.push(new top(i+1, list.get(i)));
+						System.out.println("curr-value :" + list.get(i)+" ->"+"0");
+						sb.append("0");
+					}else if(t.value == list.get(i)) {
+						stack.push(t);
+						System.out.println("curr-value :" + list.get(i)+"->"+t.index);
+					    sb.append(t.index);
+					}
+					
+				}else if(list.get(i-1) > list.get(i)) {
+					top t = stack.pop();
+					if(t.value > list.get(i-1)) {
+						stack.push(t);
+						System.out.println("curr-value :" + list.get(i)+"->"+ (i));
+						sb.append(i);
+						
+					}else if(t.value < list.get(i-1)) { //새로운 max의 등장
+						stack.push(new top(i, list.get(i-1)));
+						System.out.println("curr-value :" + list.get(i)+" ->"+"0");
+						sb.append("0");
+						
+					}else if(t.value == list.get(i-1)) {
+						stack.push(t);
+						System.out.println("curr-value :" + list.get(i)+"->"+t.index);
+					    sb.append(t.index);
+					}
+				}
+			}
+		}
+		
+		System.out.println("---------------------------------");
+		for(int i=0; i<n;i++) {
+			System.out.print(sb.charAt(i) + " ");
+		}
+		
+		sc.close();
+		
+
+	}
+
+}
